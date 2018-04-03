96c3aebc52a0246a2f2bf3f41cf22fbdf957d3e2
Zuganov <zuganov@live.com>
2018-04-02 23:25:37 -0300

    test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname+'/dist'));
 app.listen(process.env.PORT||8080);
 
 //Path Location Strategy
-app.get('/', function(req, res) {
+app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
