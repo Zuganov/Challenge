f5c0f64afe4b09a4d87b53b13e6844207ad51f03
Zuganov <zuganov@live.com>
2018-04-02 22:32:22 -0300

    new attempt to solve router problem

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-//Install express server
-const express = require('express');
-const app = express();
-const path = require('path');
-
-app.get('/*all', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+var express = require('express');
+var path = require('path');
+var router = express.Router();
+ 
+// serve angular front end files from root path
+router.use('/', express.static('app', { redirect: false }));
+ 
+// rewrite virtual urls to angular app to enable refreshing of internal pages
+router.get('*', function (req, res, next) {
+    res.sendFile(path.resolve('dist/index.html'));
 });
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+ 
+module.exports = router;
